3e614f89e26abf5e8a67192f9844422dd4a28a91
irakey <102231888+irakey@users.noreply.github.com>
2024-11-03 19:46:47 +0100

    stellar-burgers tests

---
+++ locators.py
@@ -0,0 +1,19 @@
+By.XPATH, "//span[text()='Булки']" #Таб "Булки"
+By.XPATH, "//h2[text()='Булки']" #Активный таб "Булки"
+By.XPATH, "//span[text()='Начинки']" #Таб "Начинки"
+By.XPATH, "//h2[text()='Начинки']" #Активный таб "Начинки"
+By.XPATH, "//span[text()='Соусы']"#Таб "Соусы"
+By.XPATH, "//h2[text()='Соусы']" #Активный таб "Соусы"
+By.XPATH, "//button[text()='Войти в аккаунт']" #Кнопка 'Войти в аккаунт'
+By.XPATH, "//label[text()='Email']/following-sibling::input" #Инпут ввода почты
+By.XPATH, "//label[text()='Пароль']/following-sibling::input"#Инпут ввода пароля
+By.XPATH, "//button[text()='Войти']" #Кнопка "Войти"
+By.XPATH, "//button[text()='Оформить заказ']"#Кнопка "Оформить заказ"
+By.XPATH, "//a[contains(text(),'Восстановить пароль')]"#Кнопка "Восстановить пароль"
+By.XPATH, "//a[contains(text(),'Зарегистрироваться')]"#Кнопка "Зарегистрироваться"
+By.XPATH, "//button[text()='Выход']"#Кнопка "Выход"
+By.XPATH, "//p[contains(text(),'Некорректный пароль')]"#Ошибка 'Некорректный пароль'
+By.XPATH, "//p[text()='Конструктор']"#Кнопка "Конструктор"
+By.CSS_SELECTOR, "h1.text_type_main-large"#Активный "Конструктор"
+By.XPATH, "//h1[text()='Соберите бургер']"#Заголовок 'Соберите бургер'
+By.XPATH, "//div[@class='AppHeader_header__logo__2D0X2']" #Лого сайта
